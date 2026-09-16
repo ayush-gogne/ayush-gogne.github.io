ed84f1b13e5ee1b627822afe730128a2e318d6f1
ayushgogne <ayushgogne23@gmail.com>
2026-09-16 14:20:16 -0400

    Fix project card image

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,20 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 function CardItem(props) {
   return (
-    <div id = 'projects'>
-      <li className='cards-item'>
-        <Link className='cards-item-link' to ={props.path}>
-            <figure className='cards-item-pic-wrap' data-category={props.label}>
-                <img className='cards-item-img' src={props.src} alt = 'Project'/>
-            </figure>
-            <div className='cards-item-info'>
-                <h5 className='cards-item-text'>{props.text}</h5>
-            </div>
-        </Link>
-      </li>
-    </div>
+    <li className='cards-item'>
+      <a
+        className='cards-item-link'
+        href={props.path}
+        target='_blank'
+        rel='noreferrer'
+      >
+        <figure
+          className='cards-item-pic-wrap'
+          data-category={props.label}
+        >
+          <img
+            className='cards-item-img'
+            src={props.src}
+            alt={props.text}
+          />
+        </figure>
+
+        <div className='cards-item-info'>
+          <h5 className='cards-item-text'>
+            {props.text}
+          </h5>
+        </div>
+      </a>
+    </li>
   )
 }
 
